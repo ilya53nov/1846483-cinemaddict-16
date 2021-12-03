7c2ab76c51bed6bc82fe0379949c4b43ae91be96
ilya53nov <ilya53nov@yandex.ru>
2021-12-03 18:29:49 +0300

    Модуль для отображения пользовательского рейтинга

--- src/view/user-rank-view.js
+++ src/view/user-rank-view.js
@@ -1,6 +1,36 @@
-export const createUserRankTemplate = () => (
+const userRanks = [
+  {
+    name: 'Novice',
+    min: 1,
+    max: 10,
+  },
+
+  {
+    name: 'Fun',
+    min: 11,
+    max: 20,
+  },
+
+  {
+    name: 'Movie Buff',
+    min: 21,
+    max: Infinity,
+  },
+];
+
+const getUserRank = (value) => {
+  for (const {name, min, max} of userRanks) {
+    if (value >= min && value <= max) {
+      return name;
+    }
+  }
+};
+
+export const createUserRankTemplate = (value) => (
   `<section class="header__profile profile">
-    <p class="profile__rating">Movie Buff</p>
-    <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
+    ${value > 0
+    ? `<p class="profile__rating">${getUserRank(value)}</p>
+      <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">`
+    : ''}
   </section>`
 );
