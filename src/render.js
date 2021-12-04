30e53db5457fda37fc77f5db2e7f3c12afb0e0b3
ilya53nov <ilya53nov@yandex.ru>
2021-12-04 22:13:26 +0300

    Изменена функция отрисовки

--- src/render.js
+++ src/render.js
@@ -5,6 +5,28 @@ export const RenderPosition = {
   AFTEREND: 'afterend',
 };
 
-export const renderTemplate = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
+// Функция отрисовки
+export const render = (container, element, place) => {
+  switch(place) {
+    case RenderPosition.BEFOREBEGIN:
+      container.before(element);
+      break;
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+    case RenderPosition.AFTEREND:
+      container.after(element);
+      break;
+  }
+};
+
+// Функция создания DOM элемента
+export const createElement = (template) => {
+  const newElement = document.createElement('div');
+  newElement.innerHTML = template;
+
+  return newElement.firstChild;
 };
