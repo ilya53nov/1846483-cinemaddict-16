6b505e8315c17705f45877730c88e725f2f81ead
ilya53nov <ilya53nov@yandex.ru>
2021-12-08 17:53:00 +0300

    Добавлена вспомогательная функция для создания элемента

--- src/utils.js
+++ src/utils.js
@@ -22,6 +22,15 @@ export const getRandomItem = (items) => items[getRandomInteger(items.length - 1)
 
 export const getRandomItems = (items) => items.slice(0, getRandomInteger(1, items.length - 1));
 
+export const isEscapeKey = (evt) => evt.key === 'Escape' || evt.key === 'Esc';
+
+export const getCreatedElement = (elementName, className) => {
+  const element = document.createElement(`${elementName}`);
+  element.classList.add(`${className}`);
+
+  return element;
+};
+
 export const getListTemplate = (items, template) => (
   items
     .map((item) => template(item))
