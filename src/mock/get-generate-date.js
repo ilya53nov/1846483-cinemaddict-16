99b853643027f218e13b76042055e1e0fa14e623
ilya53nov <ilya53nov@yandex.ru>
2021-12-17 19:34:17 +0300

    Изменено значение переменной FILM_COUNT

--- src/mock/get-generate-date.js
+++ src/mock/get-generate-date.js
@@ -1,7 +1,7 @@
 import { generateMovie} from './generate.js';
 import { generateFilter } from './filter.js';
 
-const FILM_COUNT = 0;
+const FILM_COUNT = 20;
 
 export const movies = Array.from({length: FILM_COUNT}, generateMovie);
 
