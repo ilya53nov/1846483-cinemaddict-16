414d02d21756a3ddd190226d233305c9f7d18409
ilya53nov <ilya53nov@yandex.ru>
2022-01-24 21:13:08 +0300

    добавлен модуль для хранения констант

--- src/render/render-footer.js
+++
@@ -1,8 +0,0 @@
-import {movies} from '../mock/get-generate-date.js';
-
-const siteFooterElement = document.querySelector('.footer');
-const footerStatistics = siteFooterElement.querySelector('.footer__statistics');
-
-export const renderFooter = () => {
-  footerStatistics.textContent = movies.length;
-};
