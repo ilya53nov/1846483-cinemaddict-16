68f2654437ed926d0731d56bd408d74d77638396
ilya53nov <ilya53nov@yandex.ru>
2022-01-24 21:21:15 +0300

    Изменена отрисовка

--- src/main.js
+++ src/main.js
@@ -1,9 +1,21 @@
-import {renderHeader} from './render/render-header.js';
-import {renderFooter} from './render/render-footer.js';
-import {renderMain} from './render/render-main.js';
+import MovieListPresenter from './presenter/movie-list-presenter.js';
+import { movies } from './mock/get-generate-date.js';
+import {render, RenderPosition} from './utils/render.js';
+import UserRankView from './view/user-rank-view.js';
+import MenuView from './view/site-menu-view.js';
+import {filters} from './mock/get-generate-date.js';
+import { getHistoryFilterCount } from './utils/utils.js';
 
-renderHeader();
+const siteMainElement = document.querySelector('.main');
+const siteHeaderElement = document.querySelector('.header');
+const siteFooterElement = document.querySelector('.footer');
+const footerStatistics = siteFooterElement.querySelector('.footer__statistics');
 
-renderMain();
+const movieListPresenter = new MovieListPresenter(siteMainElement);
 
-renderFooter();
+render(siteHeaderElement, new UserRankView(getHistoryFilterCount(filters)), RenderPosition.BEFOREEND);
+render(siteMainElement, new MenuView(filters), RenderPosition.AFTERBEGIN);
+
+movieListPresenter.init(movies);
+footerStatistics.textContent = movies.length;
+
