ca3c47b452fe60323ba53c60f602dca1ae80582d
ilya53nov <ilya53nov@yandex.ru>
2022-01-28 16:21:34 +0300

    Добален утилитарный модуль для фильтров

---
+++ src/utils/filter.js
@@ -0,0 +1,9 @@
+import { FilterType } from '../const.js';
+
+export const filter = {
+  [FilterType.ALL]: (movies) => movies.sort(),
+  [FilterType.FAVORITES]: (movies) => movies.filter((movie) => movie.userDetails.favorite),
+  [FilterType.HISTORY]: (movies) =>  movies.filter((movie) => movie.userDetails.alreadyWatched),
+  [FilterType.WATCHLIST]: (movies) =>  movies.filter((movie) => movie.userDetails.watchlist),
+
+};
