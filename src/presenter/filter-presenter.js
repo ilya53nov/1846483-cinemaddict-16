670eb9b619406a1e62d7eaa8eadf83fb65cb781c
ilya53nov <ilya53nov@yandex.ru>
2022-01-28 16:22:16 +0300

    Создан презентер для фильтров

---
+++ src/presenter/filter-presenter.js
@@ -0,0 +1,76 @@
+import FilterView from '../view/filter-view.js';
+import {render, RenderPosition, remove, replace} from '../utils/render.js';
+import {filter} from '../utils/filter.js';
+import {FilterType, UpdateType} from '../const.js';
+
+export default class FilterPresenter {
+  #filterContainer = null;
+  #filterModel = null;
+  #moviesModel = null;
+
+  #filterComponent = null;
+
+  constructor(filterContainer, filterModel, moviesModel) {
+    this.#filterContainer = filterContainer;
+    this.#filterModel = filterModel;
+    this.#moviesModel = moviesModel;
+
+    this.#moviesModel.addObserver(this.#handleModelEvent);
+    this.#filterModel.addObserver(this.#handleModelEvent);
+  }
+
+  get filters() {
+    const movies = this.#moviesModel.movies;
+
+    return [
+      {
+        type: FilterType.ALL,
+        name: 'All',
+        count: filter[FilterType.ALL](movies).length,
+      },
+      {
+        type: FilterType.FAVORITES,
+        name: 'Favorites',
+        count: filter[FilterType.FAVORITES](movies).length,
+      },
+      {
+        type: FilterType.HISTORY,
+        name: 'History',
+        count: filter[FilterType.HISTORY](movies).length,
+      },
+      {
+        type: FilterType.WATCHLIST,
+        name: 'Watchlist',
+        count: filter[FilterType.WATCHLIST](movies).length,
+      },
+    ];
+  }
+
+  init = () => {
+    const filters = this.filters;
+    const prevFilterComponent = this.#filterComponent;
+
+    this.#filterComponent = new FilterView(filters, this.#filterModel.filter);
+    this.#filterComponent.setFilterTypeChangeHandler(this.#handleFilterTypeChange);
+
+    if (prevFilterComponent === null) {
+      render(this.#filterContainer, this.#filterComponent, RenderPosition.AFTERBEGIN);
+      return;
+    }
+
+    replace(this.#filterComponent, prevFilterComponent);
+    remove(prevFilterComponent);
+  }
+
+  #handleModelEvent = () => {
+    this.init();
+  }
+
+  #handleFilterTypeChange = (filterType) => {
+    if (this.#filterModel.filter === filterType) {
+      return;
+    }
+
+    this.#filterModel.setFilter(UpdateType.MAJOR, filterType);
+  }
+}
