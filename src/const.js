3b442e87fc688d8df0ce1ec9fc1f9ec71e0470c9
ilya53nov <ilya53nov@yandex.ru>
2022-01-28 16:22:58 +0300

    Добавлены константы

--- src/const.js
+++ src/const.js
@@ -3,3 +3,22 @@ export const SortType = {
   DATE: 'date',
   RATING: 'rating',
 };
+
+export const UserAction = {
+  UPDATE_MOVIE: 'UPDATE_MOVIE',
+  ADD_COMMENT: 'ADD_COMMENT',
+  DELETE_COMMENT: 'DELETE_COMMENT',
+};
+
+export const UpdateType = {
+  PATCH: 'PATCH',
+  MINOR: 'MINOR',
+  MAJOR: 'MAJOR',
+};
+
+export const FilterType = {
+  ALL: 'all',
+  WATCHLIST: 'watchlist',
+  HISTORY: 'history',
+  FAVORITES: 'favorites',
+};
