96f9700869d0ef1f5580a16cf70dfd022ff1b9b6
ilya53nov <ilya53nov@yandex.ru>
2022-01-29 22:10:30 +0300

    Добавлен рейтинг пользователя

--- src/presenter/filter-presenter.js
+++ src/presenter/filter-presenter.js
@@ -1,17 +1,21 @@
 import FilterView from '../view/filter-view.js';
+import UserRankView from '../view/user-rank-view.js';
 import {render, RenderPosition, remove, replace} from '../utils/render.js';
 import {filter} from '../utils/filter.js';
 import {FilterType, UpdateType} from '../const.js';
 
 export default class FilterPresenter {
   #filterContainer = null;
+  #userRankContainer = null;
   #filterModel = null;
   #moviesModel = null;
 
   #filterComponent = null;
+  #userRankComponent = null;
 
-  constructor(filterContainer, filterModel, moviesModel) {
+  constructor(filterContainer, filterModel, moviesModel, userRankContainer) {
     this.#filterContainer = filterContainer;
+    this.#userRankContainer = userRankContainer;
     this.#filterModel = filterModel;
     this.#moviesModel = moviesModel;
 
@@ -46,20 +50,43 @@ export default class FilterPresenter {
     ];
   }
 
+  countHistory() {
+    return filter[FilterType.HISTORY](this.#moviesModel.movies).length;
+  }
+
   init = () => {
     const filters = this.filters;
     const prevFilterComponent = this.#filterComponent;
+    const prevUserRankComponent = this.#userRankComponent;
 
     this.#filterComponent = new FilterView(filters, this.#filterModel.filter);
+    this.#userRankComponent = new UserRankView(this.countHistory());
     this.#filterComponent.setFilterTypeChangeHandler(this.#handleFilterTypeChange);
 
+    this.#moviesModel.addObserver(this.#handleModelEvent);
+    this.#filterModel.addObserver(this.#handleModelEvent);
+
     if (prevFilterComponent === null) {
+      render(this.#userRankContainer, this.#userRankComponent, RenderPosition.BEFOREEND);
       render(this.#filterContainer, this.#filterComponent, RenderPosition.AFTERBEGIN);
+
       return;
     }
 
     replace(this.#filterComponent, prevFilterComponent);
+    replace(this.#userRankComponent, prevUserRankComponent);
     remove(prevFilterComponent);
+    remove(prevUserRankComponent);
+  }
+
+  destroy = () => {
+    remove(this.#filterComponent);
+    this.#filterComponent = null;
+
+    this.#moviesModel.removeObserver(this.#handleModelEvent);
+    this.#filterModel.removeObserver(this.#handleModelEvent);
+
+    this.#filterModel.setFilter(UpdateType.MAJOR, FilterType.ALL);
   }
 
   #handleModelEvent = () => {
