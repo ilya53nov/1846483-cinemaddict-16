de8053d59757e85923ee1a014c1320c456c52e0d
ilya53nov <ilya53nov@yandex.ru>
2022-01-29 22:11:01 +0300

    Рефакторинг

--- src/view/site-menu-view.js
+++ src/view/site-menu-view.js
@@ -1,22 +1,32 @@
 import AbstractView from './abstract-view.js';
-//import { createFilterTemplate } from './filter-view.js';
+import {MenuItem} from '../const.js';
 
 const createNavigationTemplate = () => (
   `<nav class="main-navigation">
 
-    <a href="#stats" class="main-navigation__additional">Stats</a>
+    <a href="#stats" class="main-navigation__additional" id=${MenuItem.STATISTICS}>Stats</a>
   </nav>`
 );
 
 export default class MenuView extends AbstractView{
-  //#filter = null;
-
-  //constructor(filter) {
-  //  super();
-  //  this.#filter = filter;
-  //}
 
   get template() {
     return createNavigationTemplate();
+  }
+
+  setMenuClickHandler = (callback) => {
+    this._callback.menuClick = callback;
+    this.element.addEventListener('click', this.#menuClickHandler);
+  }
+
+  #menuClickHandler = (evt) => {
+    evt.preventDefault();
+    if (evt.target.tagName === 'A') {
+      if (evt.target.id === MenuItem.STATISTICS) {
+        this._callback.menuClick(MenuItem.STATISTICS);
+      } else {
+        this._callback.menuClick(MenuItem.MOVIES);
+      }
+    }
   }
 }
