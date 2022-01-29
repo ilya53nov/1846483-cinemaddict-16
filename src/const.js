d1da2555fd215e3a0225530e03cd4ccb0c2c4257
ilya53nov <ilya53nov@yandex.ru>
2022-01-29 22:07:34 +0300

    Добавлены константы

--- src/const.js
+++ src/const.js
@@ -22,3 +22,36 @@ export const FilterType = {
   HISTORY: 'history',
   FAVORITES: 'favorites',
 };
+
+export const FilterTypeStatistics = {
+  ALL: 'all-time',
+  TODAY: 'today',
+  WEEK: 'week',
+  MONTH: 'month',
+  YEAR: 'year',
+};
+
+export const MenuItem = {
+  MOVIES: 'MOVIES',
+  STATISTICS: 'STATISTICS',
+};
+
+export const userRanks = [
+  {
+    name: 'Novice',
+    min: 1,
+    max: 10,
+  },
+
+  {
+    name: 'Fun',
+    min: 11,
+    max: 20,
+  },
+
+  {
+    name: 'Movie Buff',
+    min: 21,
+    max: Infinity,
+  },
+];
