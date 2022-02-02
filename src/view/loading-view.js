01b272ced23c9162fa1fcdeb70ff86c88d75b0f3
ilya53nov <ilya53nov@yandex.ru>
2022-02-02 09:07:29 +0300

    Добавлена вьюха для ожидания загрузки

---
+++ src/view/loading-view.js
@@ -0,0 +1,13 @@
+import AbstractView from './abstract-view.js';
+
+const createNoMovieTemplate = () => (
+  `<h2 class="films-list__title">
+  Loading...
+  </h2>`
+);
+
+export default class LoadingView extends AbstractView {
+  get template() {
+    return createNoMovieTemplate();
+  }
+}
